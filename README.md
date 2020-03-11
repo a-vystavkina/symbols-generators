53c87823d01fd3caf554911b9e714497894c9c3b
Alexandra Vystavkina <a.vystavkina@yandex.ru>
2020-03-11 15:13:25 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/7sqrsv4jkbee4af7?svg=true)](https://ci.appveyor.com/project/a-vystavkina/symbols-generators)
---
+++ src/js/Team.js
@@ -0,0 +1,31 @@
+export default class Team {
+  constructor() {
+    this.members = new Set();
+  }
+
+  add(character) {
+    if (this.members.has(character)) throw new Error('Данный персонаж добавлен ранее');
+    this.members.add(character);
+  }
+
+  addAll(...characters) {
+    for (const item of characters) {
+      this.members.add(item);
+    }
+  }
+
+  * [Symbol.iterator]() {
+    const sizeSet = this.members.size;
+    const iterTeam = this.members.values();
+    for (let currSet = 0; currSet < sizeSet; currSet += 1) {
+      yield iterTeam.next().value;
+    }
+  }
+
+  toArray() {
+    return Array.from(this.members);
+  }
+}
+
+const team = new Team();
+console.log(team);
